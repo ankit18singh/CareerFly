4bb8d8deb67a4d0c0af6a5c9702e665f0ebf04d9
ankit18singh <aj.ankitsingh@gmail.com>
2016-07-05 12:56:04 +0530

    Added Domain class Entities.

--- grails-app/domain/com/careerFly/social/Comment.groovy
+++ grails-app/domain/com/careerFly/social/Comment.groovy
@@ -2,6 +2,13 @@ package com.careerFly.social
 
 class Comment {
 
+    String body
+    Long upVotes
+    Long downVotes
+    //User author
+    //CommentEntity entity
+    Long entityID
+    
     static constraints = {
     }
 }
--- grails-app/domain/com/careerFly/social/Discussion.groovy
+++ grails-app/domain/com/careerFly/social/Discussion.groovy
@@ -2,6 +2,15 @@ package com.careerFly.social
 
 class Discussion {
 
+    String title
+    String body
+    String link
+    Long upVotes
+    Long downVotes
+    //User author
+    Set tags
+    //StoredFile file
+
     static constraints = {
     }
 }
