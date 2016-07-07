e7a41ae43932795ac6341c378b6f47c81780ce53
Sagar Jain <jain.jains.sagar@gmail.com>
2016-07-07 16:48:21 +0530

    Modified, removed constraints and applied id in enum.

--- grails-app/domain/com/careerfly/organization/interview/Interview.groovy
+++ grails-app/domain/com/careerfly/organization/interview/Interview.groovy
@@ -5,25 +5,29 @@ import com.careerfly.taggable.Tag
 import com.careerfly.user.User
 
 class Interview {
+
     Company company
     User candidate
     String jobPosition
     String qualification
-    static hasMany = [workExperience: String, technologies: Tag, tools: Tag, skills: Tag, rounds: InterviewRound]
-    Set workExperience, technologies, tools, skills, rounds
     Result result
     Date dateCreated
     Date lastUpdated
+    Set workExperience, technologies, tools, skills, rounds
+    static hasMany = [workExperience: String, technologies: Tag, tools: Tag, skills: Tag, rounds: InterviewRound]
 
     static constraints = {
-        jobPosition(matches: "[a-z,A-Z, ]+")
-        qualification(matches: "[a-z,A-Z,0-9, ]+")
-        workExperience(matches: "[a-z,A-Z,0-9, ]+")
-        skills(matches: "[a-z,A-Z,0-9, ]+")
-        rounds(matches: "[0-9]st|nd|rd|th")
     }
 }
 
 enum Result {
-    ACCEPT,ONHOLD,REJECT,DECLINE
+    ACCEPT(1),
+    ON_HOLD(2),
+    REJECT(3),
+    DECLINE(4)
+    int id
+
+    Result(int id) {
+        this.id =id
+    }
 }
--- grails-app/domain/com/careerfly/organization/interview/InterviewRound.groovy
+++ grails-app/domain/com/careerfly/organization/interview/InterviewRound.groovy
@@ -3,17 +3,16 @@ package com.careerfly.organization.interview
 import com.careerfly.user.User
 
 class InterviewRound {
+
     User candidate
     String title
-    static hasMany = [dos: String, donts: String]
-    Set dos, donts
     String experience
     int duration
     Date dateCreated
     Date lastUpdated
+    Set dos, donts
+    static hasMany = [dos: String, donts: String]
 
     static constraints = {
-        title(matches: "[a-z,A-Z,0-9, ]+")
-        duration(matches: "[0-9]")
     }
 }
