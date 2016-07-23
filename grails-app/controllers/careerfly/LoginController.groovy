5e7919ea6e735e195581dc7824125fb36f390e94
apoorvsrivastava1116 <apurvsrivastav.com@gmail.com>
2016-07-24 01:19:35 +0530

    Login controller and gsp implemented

---
+++ grails-app/controllers/careerfly/LoginController.groovy
@@ -0,0 +1,64 @@
+
+
+package careerfly
+
+import com.careerfly.user.User
+
+class LoginController {
+
+    def beforeInterceptor = {
+
+        if(session.loggedInUser){
+
+            redirect(controller: "basicProfile" , action: "index")
+
+            return false
+
+        }
+
+    }
+
+    def index() {
+
+    }
+
+    def save() {
+
+        User u
+
+        if(params.email && params.password) {
+
+            u = User.findByEmailAndPassword("${params.email}", "${params.password}")
+
+            if(u) {
+
+                session.loggedInUser = u.id
+
+                redirect(controller: "BasicProfile", action: "index" )
+
+            } else {
+
+                flash.message="Invalid user id or password"
+
+                redirect( action: "index")
+
+            }
+
+        }
+
+        else {
+
+            if (u.hasErrors()) {
+
+                render(view: 'index', model: [myUser: u])
+
+                return
+
+            }
+
+        }
+
+    }
+
+}
+
